27675b2d8606f0a977c882ce5a7211cad840cbb8
Mariaravinth <gm771855@gmail.com>
2024-07-15 20:18:33 +0530

    Commit

---
+++ EXERCISES/iava.js
@@ -0,0 +1,8 @@
+// let car;
+// switch(bugatti){
+// console.log("bugatti is my fav")}
+var arr=[27,58,97]
+      console.log(arr.length);
+      for(let i=0;i=arr.length-1;i++){
+        console.log(arr[i]);
+      }
